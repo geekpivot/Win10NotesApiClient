7eb9c70044b6050b3147900a43778345cf2aadc2
geekpivot <geekpivot@gmail.com>
2015-11-24 06:19:57 -0500

    Init Commit

---
+++ LaudableLayout/Models/Todo.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Web.Http;
+using Windows.Web.Http.Filters;
+
+namespace LaudableLayout.Models
+{
+    public class Todo
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public bool Done { get; set; }
+    }
+
+    public class TodoManager
+    {
+        public static HttpBaseProtocolFilter RootFilter { get; private set; }
+
+        public static List<Todo> GetTodoes()
+        {
+            
+            var todoes = new List<Todo>();
+            RootFilter = new HttpBaseProtocolFilter();
+
+            RootFilter.CacheControl.ReadBehavior = Windows.Web.Http.Filters.HttpCacheReadBehavior.MostRecent;
+            RootFilter.CacheControl.WriteBehavior = HttpCacheWriteBehavior.NoCache;
+            using (var client = new HttpClient(RootFilter))
+            {         
+                var response = "";
+                Task task = Task.Run(async () =>
+                {
+                    response = await client.GetStringAsync(new Uri("http://localhost:50970/api/todoes"));
+                });
+                task.Wait();
+
+                todoes = JsonConvert.DeserializeObject<List<Todo>>(response);
+
+                return todoes;
+            }
+        }
+    }
+}
---
+++ LaudableLayout/Page2.xaml.cs
@@ -0,0 +1,48 @@
+﻿using LaudableLayout.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.Web.Http;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace LaudableLayout
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class Page2 : Page
+    {
+        public List<Todo> TodoList { get; private set; }
+
+        public Page2()
+        {
+            this.InitializeComponent();
+            TodoList = TodoManager.GetTodoes();
+        }
+
+        private void HyperlinkButton_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(Page3));
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+
+        }
+        
+    }
+}
